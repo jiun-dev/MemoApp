320489e5664ec0984e5690d378d1f92645a6d9bd
jiun-dev <jiun.dev@gmail.com>
2021-07-29 22:58:48 +0900

    1

--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -7,11 +7,56 @@
 
 import UIKit
 
-class MemoFormVC: UIViewController {
-    @IBOutlet var writeMemo: UITextView!
+class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
+    var subject: String!
+    
+    @IBOutlet weak var contents: UITextView!
+    @IBOutlet weak var preview: UIImageView!
+    @IBAction func save(_ sender: Any) {
+        guard self.contents.text?.isEmpty == false else {
+            let alert = UIAlertController(title: nil, message: "내용을 입력해주세요", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        let data = MemoData()
+        
+        data.title = self.subject
+        data.contents = self.contents.text
+        data.image = self.preview.image
+        data.regdate = Data()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memolist.append(data)
+        
+        _ = self.navigationController?.popViewController(animated: true)
+        
+    }
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        self.present(picker, animated: false)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        self.preview.image = info[.editedImage] as? UIImage
+        
+        picker.dismiss(animated: false)
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let contents = textView.text as NSString
+        let length = ( (contents.length > 15) ? 15 : contents.length)
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        self.navigationItem.title = self.subject
+    }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        self.contents.delegate = self
 
         // Do any additional setup after loading the view.
     }
