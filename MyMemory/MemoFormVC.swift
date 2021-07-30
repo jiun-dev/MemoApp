860c540c5d9012b514d3324d15094fbb5dab9ccd
jiunsin <jiun.shin@lighthouse.love>
2021-07-30 19:40:41 +0900

    11

--- MyMemory/MemoFormVC.swift
+++ MyMemory/MemoFormVC.swift
@@ -62,7 +62,7 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
     }
     
 
-    /*
+  
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -70,6 +70,6 @@ class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigatio
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+  
 
 }
